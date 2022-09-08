314f040fea3d0e5a11918889512d2125e024705c
heidiuu <112929857+heidiuu@users.noreply.github.com>
2022-09-08 09:17:48 +0300

    Added incorrectAnswer check

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -40,6 +40,9 @@ class QuizQuestion extends Component {
               )}
             </ul>
           </section>
+          {this.state.incorrectAnswer ? (
+            <p className="error">Sorry, that's not right</p>
+          ) : null}
         </main>
       </div>
     );
