33da13a40e45b3ac0e965ab5b10ad0e545f6578b
heidiuu <112929857+heidiuu@users.noreply.github.com>
2022-09-08 08:59:38 +0300

    Edited handleClicks

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class QuizQuestionButton extends Component {
   handleClick() {
     console.log("QuizQuestionButton handleClick");
-    this.props.clickHandler(props.button_text);
+    this.props.clickHandler(this.props.button_text);
   }
   render() {
     this.props.clickHandler(this.props.button_text);
